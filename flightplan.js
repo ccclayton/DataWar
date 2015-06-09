1d15be4f1f22784ebfa2907ad4543844e545881d
Daniel <dgillies@dons.usfca.edu>
2015-06-09 15:53:27 -0400

    trying to fix auto deploy

--- flightplan.js
+++ flightplan.js
@@ -57,6 +57,7 @@ plan.remote(function(remote) {
 
     remote.log('Reload application');
     remote.sudo('ln -snf ~/' + tmpDir + ' ~/'+appName, {user: username});
-    remote.exec('forever stop ~/'+appName+'/'+startFile, {failsafe: true});
-    remote.exec('forever start ~/'+appName+'/'+startFile);
+    //remote.exec('pm2 stop ~/'+appName+'/'+startFile, {failsafe: true});
+    //remote.exec('pm2 delete ~/'+appName+'/'+startFile);
+    //remote.exec('pm2 start ~/'+appName+'/'+startFile);
 });
